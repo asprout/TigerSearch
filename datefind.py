894114750ab60e4aa95023850e71eebaccad74bf
Ling Dong <lingd1944@gmail.com>
2014-10-08 13:20:17 -0400

    ok

--- datefind.py
+++ datefind.py
@@ -6,7 +6,7 @@ months = month1.split("\n")
 test = "Jan 14, 2014"
 
 def vali_date(d):
-    matches = re.search('([A-Za-z]|\d{0,2})+[,/ ] *\d{1, 2}[,/ ] *d{0, 4}', d)
+    matches = re.findall('([A-Za-z]|\d{0,2})+[,/ ] *\d{1, 2}[,/ ] *d{0, 4}', d)
     print matches
 
 if __name__=="__main__":
