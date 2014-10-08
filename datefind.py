400a7b760514284202e52e0996e0a463ee47178e
Ling Dong <lingd1944@gmail.com>
2014-10-08 13:19:05 -0400

    wip

--- datefind.py
+++ datefind.py
@@ -7,7 +7,7 @@ test = "Jan 14, 2014"
 
 def vali_date(d):
     matches = re.search('([A-Za-z]|\d{0,2})+[,/ ] *\d{1, 2}[,/ ] *d{0, 4}', d)
-    return matches
+    print matches
 
 if __name__=="__main__":
     vali_date(test)
