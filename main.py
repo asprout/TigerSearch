5e54c9aeadff0ae6fe0cb20e1d6b42e5f13f9bf3
Alex Libman <kingalex11235@yahoo.com>
2014-10-12 12:43:35 -0400

    getNames(txt,n) now returns top n names that occur in txt in of order of occurrences

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from namefind import getNames
 from bs4 import BeautifulSoup
 import google
 
-links = google.search("Who is the current principal of Stuyvesant High School?",num=1,start=0,stop=0,pause=2.0)
+links = google.search("Who led Great Britain during World War 2?",num=1,start=0,stop=0,pause=2.0)
 
 txt = BeautifulSoup(google.get_page(links.next()))
-print(getNames(txt.getText()))
+print(getNames(txt.getText(),5))
